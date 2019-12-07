794fdd015d67872a3ca8ec7055655101650bb282
Alex <koxelez@users.noreply.github.com>
2019-12-07 11:47:11 -0400

    arreglado Perfil.js

--- componentes/Perfil.js
+++ componentes/Perfil.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
-import {ScrollView, Image, View } from 'react-native'
+import {ScrollView, Image, View, StyleSheet} from 'react-native'
 import { Container, Card,CardItem,Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base'
 
 class Perfil extends Component {
     render() {
         return (
+          <View>
            <Text>actualizado</Text>
            <Text>Luis Alejandro Dice "Hola"</Text>
-        )
+           </View>
+        );
     }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
 
 export default Perfil;
