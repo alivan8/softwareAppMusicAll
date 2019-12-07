53b677a640664440223ddf47c2977105880283ca
Alex <koxelez@users.noreply.github.com>
2019-12-07 16:06:17 -0400

    lista de ejercicios (toys) usando "Card Image" de NativeBase
    véase:
    https://docs.nativebase.io/Components.html#card-image-headref

--- componentes/ListaEjercicios.js
+++ componentes/ListaEjercicios.js
@@ -5,49 +5,59 @@ import {
   View,
   StyleSheet,
   TouchableOpacity,
-  ImageBackground,
   Text
 } from 'react-native';
-// import { Container, Header, Card, CardItem, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+ import { Container, Header, Card, CardItem, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon} from 'native-base';
 
 export default class ListaEjercicios extends Component {
   render() {
     return (
       <ScrollView>
         <View style={styles.container}>
-          <Text>La imagen de teclado debería tener dimensiones que lleven a su parent.</Text>
           {/* card de Teclado */}
           <TouchableOpacity onPress={() => { this.props.navigation.navigate('PianoToy') }}>
-            <ImageBackground
-              source={{ uri: 'toys_teclado' }}
-              style={styles.imageCard}
-              resizeMode='cover'
-            >
-              <Text style={styles.cardTitle}>Teclado</Text>
-            </ImageBackground>
+            <Card>
+              <CardItem cardBody>
+                <Image
+                  source={{ uri: 'toys_teclado' }}
+                  style={styles.cardImage}
+                />
+              </CardItem>
+              <CardItem>
+                <Text style={styles.cardTitle}>Teclado/Piano</Text>
+              </CardItem>
+            </Card>
           </TouchableOpacity>
 
 
           {/* card de Guitarra */}
           <TouchableOpacity onPress={() => { this.props.navigation.navigate('GuitarToy') }}>
-            <ImageBackground
-              source={{ uri: 'toys_guitarra' }}
-              style={styles.imageCard}
-              resizeMode='cover'
-            >
-              <Text style={styles.cardTitle}>Guitarra</Text>
-            </ImageBackground>
+            <Card>
+              <CardItem cardBody>
+                <Image
+                  source={{ uri: 'toys_guitarra' }}
+                  style={styles.cardImage}
+                />
+              </CardItem>
+              <CardItem>
+                <Text style={styles.cardTitle}>Guitarra</Text>
+              </CardItem>
+            </Card>
           </TouchableOpacity>
 
           {/* card de Batería */}
           <TouchableOpacity onPress={() => { this.props.navigation.navigate('DrumToy') }}>
-            <ImageBackground
-              source={{ uri: 'toys_bateria' }}
-              style={styles.imageCard}
-              resizeMode='cover'
-            >
-              <Text style={styles.cardTitle}>Batería</Text>
-            </ImageBackground>
+            <Card>
+              <CardItem cardBody>
+                <Image
+                  source={{ uri: 'toys_bateria' }}
+                  style={styles.cardImage}
+                />
+              </CardItem>
+              <CardItem>
+                <Text style={styles.cardTitle}>Batería</Text>
+              </CardItem>
+            </Card>
           </TouchableOpacity>
         </View>
       </ScrollView>
@@ -58,17 +68,23 @@ export default class ListaEjercicios extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center'
+    // margin: 10,
+    // alignItems: 'fl',
   },
-  imageCard: {
-    //flex: 1,
-    width: '98%',
+  card: {
+    // flexDirection: 'column'
+    //flex: 0,
+    // width: '100%'
+  },
+  cardImage: {
+    flex: 1,
+    width: null,  // según documentación de NativeBase Card Image
     height: 200,
-    borderRadius: 5
+    // borderRadius: 5
   },
   cardTitle: {
-    color: 'yellow',
-    fontSize: 30,
+    // color: 'silver',
+    fontSize: 20,
     // marginTop: -120,
     marginLeft: 20
   }
